fafd975f4f99d62fc75d6cfd8bb8b65b39d029f3
zxt1996 <15625144271@163.com>
2020-08-21 11:18:50 +0800

    中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
+
+// 中间件
+import { LogMiddleware } from './middlewares/log.middleware';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure (consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LogMiddleware)
+      // 排除指定路由
+      .exclude(
+        { path: 'user/login', method: RequestMethod.GET}
+      )
+      // .forRoutes('user')
+      .forRoutes({ path: 'user', method: RequestMethod.GET })
+  }
+}
